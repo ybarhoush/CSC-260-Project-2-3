04418224ea1968f06704cf684aa18da15451a81e
Yazan Barhoush <ybarhoush@gmail.com>
2017-02-23 22:53:28 -0500

    updated tutorial sets

--- view/TutorialSets.java
+++ view/TutorialSets.java
@@ -5,13 +5,12 @@ import javax.swing.*;
 /**
  * JFrame view to cycle through highlighted sets.
  */
-public class TutorialSets extends JPanel {
+public class TutorialSets extends JFrame {
 
-    private JFrame frame;
     private JLabel message;
 
     public TutorialSets(int n){
-//        setLayout(new BoxLayout(getContentPane() , BoxLayout.Y_AXIS));
+        setLayout(new BoxLayout(getContentPane() , BoxLayout.Y_AXIS));
         String messageNum;
         if (n == 0){
             messageNum = "There are no sets in this layout. Please close this window and click next game.";
@@ -23,16 +22,7 @@ public class TutorialSets extends JPanel {
             messageNum = "There are " + n + " sets in the layout. Please watch as we highlight them for you.";
         }
         message = new JLabel(messageNum);
-//        getContentPane().add(message);
-
-
-        frame = new JFrame("Sets");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(200, 200);
-        frame.pack();
-        frame.setVisible(true);
-
-
-//        pack();
+        getContentPane().add(message);
+        pack();
     }
 }
