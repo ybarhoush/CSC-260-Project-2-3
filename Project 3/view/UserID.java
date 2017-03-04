85626085a4fa47d3b282cc64c794bd1a58370e2c
Yazan Barhoush <ybarhoush@gmail.com>
2017-03-04 01:10:01 -0500

    finished the classes and window that first appears and askes user for a 3 digit id

--- Project 3/view/UserID.java
+++ Project 3/view/UserID.java
@@ -1,78 +1,61 @@
 package view;
 
-import com.sun.org.apache.regexp.internal.RE;
 import main.Editor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-
-public class UserID extends JPanel {
-
-    private static final String MEMORY_LABEL = "Welcome to Memory";
-    private static final String RETURNING_PLAYER_BUTTON = "Returning Player";
-    private static final String NEW_PLAYER_BUTTON = "New Player";
+/**
+ * Created by David on 3/3/17.
+ */
+public class UserID extends JPanel{
+    private static final String ENTER_BUTTON = "Enter";
+    private static final String USER_ID = "Enter Your Three Digit User ID:";
 
     private Editor mainListener;
-    private JButton returningPlayer;
-    private JButton newPlayer;
-    private JPanel mem;
+    private JPanel p1;
+    private JLabel image1;
+    private JLabel image2;
+    private JTextField threeDigitID;
+    private JButton buttonOne;
+    private JLabel id;
 
-    /**
-     * Constructor for creating a new main.Editor Menu.
-     * @param mainListener Listener callbacks for the main.Editor.
-     */
+
+
     public UserID(Editor mainListener) {
         this.mainListener = mainListener;
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        initGUI();
+
+        p1 = new JPanel();
+        p1.setLayout(new BoxLayout(p1, BoxLayout.PAGE_AXIS));
+
+        image1 = new JLabel(new ImageIcon("memory-challenge.png"));
+        setCenterAlignment(image1);
+        p1.add(image1);
+
+        id = new JLabel(USER_ID);
+
+        threeDigitID = new JTextField(3);
+        threeDigitID.setDocument(new JTextFieldLimitThreeChars());
+
+        JPanel p2 = new JPanel();
+        GroupLayout layout = new GroupLayout(p2);
+        layout.setHorizontalGroup(
+                layout.createSequentialGroup().addComponent(id).addComponent(threeDigitID)
+        );
+        p1.add(p2);
+
+        buttonOne = new JButton(ENTER_BUTTON);
+        setCenterAlignment(buttonOne);
+        p1.add(buttonOne);
+
+        image2 = new JLabel(new ImageIcon("memory_game.jpg"));
+        setCenterAlignment(image2);
+        p1.add(image2);
+
+        add(p1);
     }
-
-    /**
-     * Initializes the GUI
-     */
-    private void initGUI() {
-
-        JLabel memory = new JLabel(MEMORY_LABEL);
-        memory.setFont(new Font(getFont().getName(), Font.BOLD, 50));
-        setCenterAlignment(memory);
-        add(memory);
-
-        returningPlayer = new JButton(RETURNING_PLAYER_BUTTON);
-        returningPlayer.addActionListener(new ReturningPlayerButtonListener());
-
-        newPlayer = new JButton(NEW_PLAYER_BUTTON);
-        newPlayer.addActionListener(new NewPlayerButtonListener());
-
-        mem = new JPanel();
-        mem.add(returningPlayer);
-        mem.add(newPlayer);
-        add(mem, BorderLayout.CENTER);
-    }
-
     private void setCenterAlignment(JComponent component) {
         component.setAlignmentX(Component.CENTER_ALIGNMENT);
         component.setAlignmentY(Component.CENTER_ALIGNMENT);
-    }
-
-    /**
-     * Returning Player button is pressed.
-     */
-    private class ReturningPlayerButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-//            ;
-        }
-    }
-
-
-    /**
-     * New Player button is pressed.
-     */
-    private class NewPlayerButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-//            ;
-        }
     }
 }
