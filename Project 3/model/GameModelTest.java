a95b8d22ea6b530752960e0b0e147ebec551ee02
David Koval <kovald@union.edu>
2017-03-04 21:15:39 -0500

    record if user id is new player or returning player

--- Project 3/model/GameModelTest.java
+++ Project 3/model/GameModelTest.java
@@ -1,4 +1,7 @@
 package model;
+import readAndWrite.ReadFromFile;
+import view.UserID;
+
 import java.awt.*;
 import java.util.*;
 import java.util.List;
@@ -17,10 +20,19 @@ public class GameModelTest {
 //
 //    }
 
+//    @org.junit.Test
+//    public void test_Length(){
+//        System.out.println("Test if deck has 36 pairs \n -------------------");
+//        DeckModel k = new DeckModel();
+//        assertEquals("The number of pairs in this deck of 72 cards is", 36, k.hasThirtySixPairs());
+//    }
+
     @org.junit.Test
-    public void test_Length(){
-        System.out.println("Test if deck has 36 pairs \n -------------------");
-        DeckModel k = new DeckModel();
-        assertEquals("The number of pairs in this deck of 72 cards is", 36, k.hasThirtySixPairs());
+    public void testReader(){
+        System.out.println("Test if reader can read properly \n --------------");
+        ReadFromFile checkIfPlayerExists = new ReadFromFile("Players/AllPlayers.txt");
+        ArrayList<String> allPlayers = checkIfPlayerExists.returnLine();
+        assertEquals("the method works", true, allPlayers.contains("896"));
+
     }
 }
