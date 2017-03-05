ae8b943bff283ec42e623ad9848bc540d308ad61
David Koval <kovald@union.edu>
2017-03-04 19:32:50 -0500

    added read/write files

---
+++ Project 3/readAndWrite/InRead.java
@@ -0,0 +1,17 @@
+package readAndWrite;
+
+import java.io.FileReader;
+
+/**
+ * Created by David on 3/4/17.
+ */
+public class InRead {
+
+    try{
+        FileReader fr = new FileReader("AllPlayers.txt");
+        BufferedReder br = new BufferedReader(fr);
+
+        String str;
+        
+    }
+}
