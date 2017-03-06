76f29106d691a42faa53a90f4979465b04e43275
Yazan Barhoush <barhousy@union.edu>
2017-03-06 15:03:03 -0500

    cardView modified to support a face

--- Project 3/view/CardView.java
+++ Project 3/view/CardView.java
@@ -19,8 +19,6 @@ public class CardView extends JPanel {
     private static final Border selectedBorder = BorderFactory.createStrokeBorder(new BasicStroke(50.0f));
 
     private CardModel cardModel;
-    private Color highlightColor;
-    private Color defaultColor;
     private List<Shape> shapeViewList;
 
     /**
@@ -31,24 +29,25 @@ public class CardView extends JPanel {
         super();
         this.cardModel = cardModel;
         this.shapeViewList = new ArrayList<>();
-        this.defaultColor = Color.WHITE;
 
         setLayout(new GridLayout(1, 3));
         setBackground(Color.WHITE);
         addShapes();
         toggleSelection();
+
     }
 
     /**
      * Adds shapes to the card.
      */
-    private void addShapes() {
+    public void addShapes() {
         for (int i = 0; i < cardModel.getShapeNum(); i++) {
             Shape shape = ShapeFactory.buildShape(cardModel);
             add(shape);
             shapeViewList.add(shape);
         }
     }
+
 
     /**
      * Returns the card that is represented view.
@@ -59,41 +58,17 @@ public class CardView extends JPanel {
     }
 
     private void toggleSelection() {
-        if (cardModel.isSelected()) {
+        if (cardModel.isSelected() == false) {
             setBorder(selectedBorder);
-        } else {
-            setBorder(nullBorder);
         }
     }
 
-
-    /**
-     * Highlights a card when being used
-     * in the algorithm that displays a hint
-     * or a set of cards to the player.
-     */
-    public void highlight() {
-        setBackground(highlightColor);
-        for (Shape shapeView : shapeViewList) {
-            shapeView.setHighlightColor(highlightColor);
-        }
-    }
-
-    /**
-     * Unhighlights a card.
-     */
-    public void unHighlight() {
-        setHighlightColor(defaultColor);
-        highlight();
-    }
-
-
-    /**
-     * Highlights by changing the background color.
-     * @param highlightColor Color to highlight with
-     */
-    public void setHighlightColor(Color highlightColor) {
-        this.highlightColor = highlightColor;
-    }
+//    private void toggleSelection() {
+//        if (cardModel.isSelected()) {
+//            setBorder(selectedBorder);
+//        } else {
+//            setBorder(nullBorder);
+//        }
+//    }
 }
 
