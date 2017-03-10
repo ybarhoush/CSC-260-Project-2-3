a1e183086cbcd22e163401fda4a38a010be126f0
Dae Kwang Lee <Dae Kwang Lee>
2017-03-09 20:05:10 -0500

    Added documentations to Model pacakge.

--- Project 3/model/CardModel.java
+++ Project 3/model/CardModel.java
@@ -1,51 +1,127 @@
 package model;
 
 import java.awt.*;
+
 /**
- *
- * This class is a card model.
+ * CardModel.java
+ * Represents a card to be used in the memory game.
  */
 public class CardModel {
 
 
-    /** All the shades that can be displayed on a card */
-    public enum Shade {SOLID, OPEN}
+    /**
+     * All the shades that can be displayed on a card.
+     */
+    public enum Shade {
+        SOLID, OPEN
+    }
 
-    /** All the shapes that can be displayed on a card */
-    public enum Shape {DIAMOND, CIRCLE, SQUARE}
+    /**
+     * All the shapes that can be displayed on a card.
+     */
+    public enum Shape {
+        DIAMOND, CIRCLE, SQUARE
+    }
 
     private int shapeNum;       //number of shapes on a card
     private Shape shape;        //shape on the card
     private Shade shade;        //shade on the shapes on the card
-    private Color color;        //color of shapes on cardz
+    private Color color;        //color of shapes on card
     private boolean isSelected; //if card is selected by player
+
     /**
      * Creates a new cardModel given a number that will represent
      * the number of shapes the card holds, shape on the card, shade
      * that all the shapes share, color of shapes.
-     * @param n number of shapes
-     * @param s shape(s) on card
+     *
+     * @param n  number of shapes
+     * @param s  shape(s) on card
      * @param ss shading on shapes
-     * @param c color of shapes
+     * @param c  color of shapes
      */
-    protected CardModel(int n, Shape s, Shade ss, Color c){
+    protected CardModel(int n, Shape s, Shade ss, Color c) {
         this.shapeNum = n;
         this.shape = s;
         this.shade = ss;
         this.color = c;
     }
 
-    /** getter methods */
-    public int getShapeNum(){return this.shapeNum;}
-    public Shape getShape (){return this.shape;}
-    public Shade getShade (){return this.shade;}
-    public Color getColor(){return this.color;}
-    public boolean isSelected (){return this.isSelected;}
+    /**
+     * Returns the number of shapes on a card.
+     * @return the number of shapes on a card
+     */
+    public int getShapeNum() {
+        return this.shapeNum;
+    }
 
-    /** setter methods */
-    public void setShapeNum (int n){this.shapeNum = n;}
-    public void setShape (Shape s){this.shape = s;}
-    public void setShade (Shade s){this.shade = s;}
-    public void setColor (Color c) {this.color = c;}
-    public void unSelect () {this.isSelected = !this.isSelected;}
+    /**
+     * Returns the shape on a card.
+     * @return the shape on a card
+     */
+    public Shape getShape() {
+        return this.shape;
+    }
+
+    /**
+     * Returns the shade of the shape on a card.
+     * @return the shade of the shape on a card
+     */
+    public Shade getShade() {
+        return this.shade;
+    }
+
+    /**
+     * Returns the color of a card.
+     * @return the color of a card
+     */
+    public Color getColor() {
+        return this.color;
+    }
+
+    /**
+     * Returns 1 if the card is selected by the user.
+     * @return 0 or 1
+     */
+    public boolean isSelected() {
+        return this.isSelected;
+    }
+
+    /**
+     * Sets the number of shapes on a card.
+     * @param n the number of shapes
+     */
+    public void setShapeNum(int n) {
+        this.shapeNum = n;
+    }
+
+    /**
+     * Sets the shape on a card.
+     * @param s the shape object
+     */
+    public void setShape(Shape s) {
+        this.shape = s;
+    }
+
+    /**
+     * Sets the shade of the shape on a card.
+     * @param s the shade object
+     */
+    public void setShade(Shade s) {
+        this.shade = s;
+    }
+
+    /**
+     * Sets the color of the shape on a card.
+     * @param c the color object
+     */
+    public void setColor(Color c) {
+        this.color = c;
+    }
+
+    /**
+     * Sets a card to be unselected.
+     */
+    public void unSelect() {
+        this.isSelected = !this.isSelected;
+    }
 }
--- Project 3/model/DeckModel.java
+++ Project 3/model/DeckModel.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * This class creates a deck of cards which will be used
- * to play the game of set.
+ * DeckModel.java
+ * Represents a deck of cards, which will be used to play the game of set.
  */
 public class DeckModel {
 
@@ -18,7 +18,7 @@ public class DeckModel {
     /**
      * Creates a new deck object.
      */
-    public DeckModel(){
+    public DeckModel() {
         this.deck = new ArrayList<>(DECK_LENGTH);
         createDeck();
         this.index = DECK_LENGTH - 1;
@@ -27,24 +27,24 @@ public class DeckModel {
     /**
      * Shuffles the deck.
      */
-    public void shuffle(){
+    public void shuffle() {
         Collections.shuffle(this.deck);
     }
 
     /**
-     * This method produces 36 unique cards (no two cards are a pair).
+     * Produces 36 unique cards (no two cards are a pair).
      * Then it duplicates 36 unique cards to ensure that each card only
      * has one duplicate (together they form a pair).
      */
-    private void createDeck(){
+    private void createDeck() {
         Color[] colors = new Color[]{Color.orange, Color.magenta, Color.blue};
-        for (int r = 0; r < 2; r++){
+        for (int r = 0; r < 2; r++) {
             int i = 0;
-            for (int number = 1; number <= 2; number++){
-                for (CardModel.Shape shape : CardModel.Shape.values()){
-                    for (CardModel.Shade shade : CardModel.Shade.values()){
-                        for (Color color : colors){
-                            this.deck.add(i+(r*(DECK_LENGTH/2)), new CardModel(number, shape, shade, color));
+            for (int number = 1; number <= 2; number++) {
+                for (CardModel.Shape shape : CardModel.Shape.values()) {
+                    for (CardModel.Shade shade : CardModel.Shade.values()) {
+                        for (Color color : colors) {
+                            this.deck.add(i + (r * (DECK_LENGTH / 2)), new CardModel(number, shape, shade, color));
                             i++;
                         }
                     }
@@ -54,13 +54,13 @@ public class DeckModel {
     }
 
 
-
     /**
      * Deals one card from the deck.
+     *
      * @return first card in deck that has not yet been seen by player
      */
-    public CardModel dealOne(){
-        if (!isEmpty()){
+    public CardModel dealOne() {
+        if (!isEmpty()) {
             CardModel returnCard = deck.remove(this.index);
             this.index--;
             return returnCard;
@@ -69,10 +69,12 @@ public class DeckModel {
     }
 
     /**
-     * @return true iff deck is empty
+     * Returns 1 if the deck is empty.
+     *
+     * @return true iff the deck is empty
      */
-    private boolean isEmpty(){
-        if (this.deck.size() > 0){
+    private boolean isEmpty() {
+        if (this.deck.size() > 0) {
             return false;
         }
         return true;
@@ -80,61 +82,112 @@ public class DeckModel {
 
 
     /**
-     * Beyond this point are tester methods
-     * @return
+     * Tester methods that checks if the pairs add up to 36.
+     *
+     * @return the number of pairs
      */
-    public int hasThirtySixPairs(){
+    public int hasThirtySixPairs() {
         int q = 0;
-        for (int i = 0; i < 72; i++){
-            for (int j = (i+1); j < this.deck.size(); j++){
+        for (int i = 0; i < 72; i++) {
+            for (int j = (i + 1); j < this.deck.size(); j++) {
                 CardModel a = this.deck.get(i);
                 CardModel b = this.deck.get(j);
 
-                if (isPair(a, b)){
+                if (isPair(a, b)) {
                     q++;
                 }
             }
         }
         return q;
     }
-    public boolean halfDeckIsUnique(){
-        if (this.halfDeckHasNoDuplicate() == 0){
+
+    /**
+     * Tester method that returns 1 if there is no duplicate cards in the deck.
+     *
+     * @return 0 or 1
+     */
+    public boolean halfDeckIsUnique() {
+        if (this.halfDeckHasNoDuplicate() == 0) {
             return true;
         }
         return false;
     }
-    public int halfDeckHasNoDuplicate(){
-        int r = 0;
-        for (int i = 0; i<36; i++){
-            for (int j = (i+1); j < 36; j++){
-                    CardModel a = this.deck.get(i);
-                    CardModel b = this.deck.get(j);
 
-                    if (isPair(a, b)){
-                        r++;
-                        break;
-                    }
+    /**
+     * Tester method that returns the number of duplicate cards in the half of the deck.
+     *
+     * @return the number of duplicate cards int he half of the deck
+     */
+    public int halfDeckHasNoDuplicate() {
+        int r = 0;
+        for (int i = 0; i < 36; i++) {
+            for (int j = (i + 1); j < 36; j++) {
+                CardModel a = this.deck.get(i);
+                CardModel b = this.deck.get(j);
+
+                if (isPair(a, b)) {
+                    r++;
+                    break;
+                }
             }
             break;
         }
         return r;
     }
 
-
-    private boolean isPair(CardModel a, CardModel b){
+    /**
+     * Checks if the two selected cards are a pair or not.
+     * Returns 1 if they are a pair.
+     *
+     * @param a Card object
+     * @param b Card object
+     * @return 0 or 1
+     */
+    private boolean isPair(CardModel a, CardModel b) {
         return (checkColor(a, b) && checkShape(a, b) && checkShade(a, b) && checkNum(a, b));
     }
 
-    private boolean checkColor(CardModel a, CardModel b){
+    /**
+     * Checks the color of two cards. Returns 1 if they have the same color.
+     *
+     * @param a Card object
+     * @param b Card object
+     * @return 0 or 1
+     */
+    private boolean checkColor(CardModel a, CardModel b) {
         return (a.getColor() == b.getColor());
     }
-    private boolean checkShape(CardModel a, CardModel b){
+
+    /**
+     * Checks the shape of two cards. Returns 1 if they have the same shape.
+     *
+     * @param a Card object
+     * @param b Card object
+     * @return 0 or 1
+     */
+    private boolean checkShape(CardModel a, CardModel b) {
         return (a.getShape() == b.getShape());
     }
-    private boolean checkShade(CardModel a, CardModel b){
+
+    /**
+     * Checks the shade of the shape of two cards. Returns 1 if they have the same shade.
+     *
+     * @param a Card object
+     * @param b Card object
+     * @return 0 or 1
+     */
+    private boolean checkShade(CardModel a, CardModel b) {
         return (a.getShade() == b.getShade());
     }
-    private boolean checkNum(CardModel a, CardModel b){
+
+    /**
+     * Checks the number of the shapes on two cards. Returns 1 if they have the same number of shapes.
+     *
+     * @param a Card object
+     * @param b Card object
+     * @return 0 or 1
+     */
+    private boolean checkNum(CardModel a, CardModel b) {
         return (a.getShapeNum() == b.getShapeNum());
     }
 }
